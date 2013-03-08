724cfdb6021ec422006fef8d2488cccdbf2feb55
Jon Swanson <jswan2@uw.edu>
2013-03-08 15:10:31 -0800

    tweaked the header some more to behave better at certain widths (large and approaching mobile)

--- banner.php
+++ banner.php
@@ -13,8 +13,9 @@
 		<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'twentyeleven' ); ?>"><?php _e( 'Skip to primary content', 'twentyeleven' ); ?></a></div>
 		<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to sidebar content', 'twentyeleven' ); ?>"><?php _e( 'Skip to sidebar content', 'twentyeleven' ); ?></a></div>
 
-		<a class="patch" href="http://www.uw.edu" title="University of Washington">University of Washington</a>
-		<a class="wordmark" <?php custom_wordmark(); ?> href="<?php echo is_custom_wordmark() ? home_url('/') : '//www.washington.edu'; ?>">University of Washington</a>
+		<a class="patch hidden-phone" href="http://www.uw.edu" title="University of Washington">University of Washington</a>
+		<a class="wordmark hidden-phone" <?php custom_wordmark(); ?> href="<?php echo is_custom_wordmark() ? home_url('/') : '//www.washington.edu'; ?>">University of Washington</a>
+		<a class="wordmark visible-phone" <?php custom_wordmark(); ?> href="<?php echo is_custom_wordmark() ? home_url('/') : '//www.washington.edu'; ?>">University of Washington</a>
 		<a title="Show search" role="button" href="#searchicon-wrapper" id="searchicon-wrapper" aria-haspopup="true">Search</a>
 
       <?php get_template_part('uw-search'); ?>
@@ -24,7 +25,7 @@
 </div>
 
 <div id="thin-strip">
-	<div>
+	<div class='container'>
 		<ul role="navigation">
 			<li><a href="http://www.washington.edu/">UW Home</a></li>
 			<li><a href="http://www.washington.edu/home/directories.html">Directories</a></li>
