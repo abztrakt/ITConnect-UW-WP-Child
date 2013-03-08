0aea2058e3f061874b8ece94e2f90c1eafe6deb8
Craig M. Stimmel <cstimmel@uw.edu>
2013-03-08 14:03:23 -0800

    Wrapped nav and footer in .container divs so they match the fixed content area.

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php wp_footer(); ?> 
+
+
+<div id="footerBG">
+    <div id="footer" role="navigation" aira-label="Global Footer Menu">
+        <div class="container">
+        <?php uw_footer_menu(); ?>
+        </div>
+    </div>
+</div>
+
+
+<div id="footer-main" role="footer">
+  <div class="container">
+      <div id="footer-right">
+        <a href="http://www.seattle.gov/">Seattle, Washington</a>
+      </div>
+          <ul role="footer">
+            <li><a href="http://www.washington.edu/home/siteinfo/form">Contact Us</a></li>
+            <li><a href="http://www.washington.edu/jobs">Jobs</a></li>
+            <li><a href="http://myuw.washington.edu/">My UW</a></li>
+            <li><a href="http://www.washington.edu/admin/rules/wac/rulesindex.html">Rules Docket</a></li>
+            <li><a href="http://www.washington.edu/online/privacy">Privacy</a></li>
+            <li><a href="http://www.washington.edu/online/terms">Terms</a></li>
+          </ul>
+      <div id="footer-left">
+        <a href="http://www.washington.edu/">&copy; <?php echo date('Y'); ?> University of Washington</a>
+      </div>
+  </div> <!-- .container -->
+</div>
+
+
+</body>
+</html>
