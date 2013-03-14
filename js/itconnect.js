d709e32c8adafbba8e7c29a2f7d2fa4057371bd2
Jon Swanson <jswan2@uw.edu>
2013-03-13 17:11:07 -0700

    ITC-57 Additional tweaks to sidebar, specifically header upppercase and sidebar height on mobile

--- js/itconnect.js
+++ js/itconnect.js
@@ -2,13 +2,12 @@
 
     var sw = $(window).width(),
         sh = $(window).height(),
-        breakpoint = 650,
+        breakpoint = 768,
         speed = 800,
         mobile = true;
 
 	$(document).ready(function() {
 		checkMobile();
-		setOffCanvasHeight();
 	});
 
 	$(w).resize(function(){ //Update dimensions on resize
@@ -20,20 +19,25 @@
 	//Check if Mobile
 	function checkMobile() {
 		mobile = (sw > breakpoint) ? false : true;
-
+        
 		if (!mobile) { //Desktop
-
 		} else { //Mobile
-
+            
 		}
+		setOffCanvasHeight(mobile);
 	}
 
-	function setOffCanvasHeight() {
+	function setOffCanvasHeight(mobile) {
 
     	// set the offcanvas (sidebar) height equal to the content for now... for mobile, might want to do something sweet
     	// like setting it to the height of the viewport (i.e. facebook)
-    	var contentH = $("#content").height();
-    	$("#sidebar").height(contentH);
+        if (mobile) {
+            $("sidebar").height('auto');
+        }
+        else {
+    	    var contentH = $("#content").height();
+    	    $("#sidebar").height(contentH);
+        }
 
 	}
 
