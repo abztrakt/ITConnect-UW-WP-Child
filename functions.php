a346e115a8461bb0c89549587734ba20b80f7b7a
Jon Swanson <jswan2@uw.edu>
2013-06-14 09:53:44 -0700

    refs ITC-251: Overrode marketing's uw_setup method so we aren't loading cherries as the default header

--- functions.php
+++ functions.php
@@ -1,4 +1,33 @@
 <?
+
+if ( ! function_exists( 'uw_setup' ) ):  
+
+  function uw_setup() 
+  {
+
+      add_theme_support( 'automatic-feed-links' );
+      add_theme_support( 'post-thumbnails' );
+
+    add_image_size( 'Thimble', 50, 50, true );
+    add_image_size( 'Sidebar', 250, 9999, false );
+    add_image_size( 'Body Image', 300, 9999, false );
+    add_image_size( 'Full Width', 620, 9999, false );
+
+    add_image_size( 'thumbnail-large', 300, 300, true );
+
+      register_nav_menu( 'primary', __( 'Primary Menu', 'uw' ) );
+      register_nav_menu( 'footer', __( 'Footer Menu', 'uw' ) );
+
+    define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1280 ) );
+    define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 215 ) );
+    
+      add_theme_support( 'custom-header', array( 'random-default' => true ) );
+      //add_custom_image_header( 'uw_header_style', 'uw_admin_header_style', 'uw_admin_header_image' );
+    
+  }
+
+endif;
+
 if ( ! function_exists( 'uw_enqueue_default_styles' ) ):
 /**
  * This is where all the CSS files are registered
