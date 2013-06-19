e31ed06661d5bea756d0e343dff18a79e96e65cb
Jon Swanson <jswan2@uw.edu>
2013-06-19 09:47:13 -0700

    refs ITC-242: added outages alert to the front page

--- front-page.php
+++ front-page.php
@@ -5,6 +5,7 @@
 
 			<div class="row">
 
+                <?php include('outages.php'); ?>
 				<div id="home_main" class="span9" <?php custom_main_image();?>>
                     <span id='overlay' class='visible-phone'></span>
         			<?php while ( have_posts() ) : the_post(); ?>
