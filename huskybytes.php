1647b2f67c7e835b648bf52968288ff8edb8f313
Stephy Ma <stephyma@uw.edu>
2013-11-12 15:36:25 -0800

    created huskybytes post template fix ITC-513

---
+++ huskybytes.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Template Name Posts: HuskyBytes
+*/
+?>
+
+<?php get_header(); ?>
+    <div id="wrap"> 
+		<div id="primary">
+			<div id="content" role="main" class="it_container">
+			
+						
+			<div class="row row-offcanvas row-offcanvas-left">
+				<div id="secondary" class="span3 sidebar-offcanvas" role="complementary">
+					<div class="stripe-top"></div><div class="stripe-bottom"></div>				
+                    <div id="sidebar">
+					    <?php dynamic_sidebar('huskybytes-sidebar'); ?>
+                        <h2 class="widgettitle">
+                            <span>Resources</span>
+                        </h2>
+                        <ul>
+                            <li><a href="http://myuw.washington.edu/">MyUW</a></li>
+                            <li><a href="http://www.lib.washington.edu/">University Libraries</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+				
+                <p id="mobile_image" class="span9 visible-phone" <?php custom_main_image();?>>
+                    <span id='overlay'></span>
+                    <span class='category'>
+                    <?php $categories = get_the_category();
+                    echo $categories[0]->cat_name;
+                    ?>
+                    </span>
+                </p>
+                <?php include('outages.php'); ?>
+                <p class="pull-left visible-phone"><a href="#sidebar" class="btn btn-primary btn-offcanvas" data-toggle="offcanvas"></a><span>All News</span></p>
+				<div id='tertiary' class="span9">
+					<span id="arrow-mark"></span>
+						
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                        <h5 class="date"><?php echo get_the_date(); ?></h5>
+
+						<header class="entry-header">
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+                        </header><!-- .entry-header -->
+					
+						<div class="media">
+                            <?php if ( has_post_thumbnail() ) : ?>
+                            <span class='pull-left'>
+                                <?php the_post_thumbnail(); ?>
+                            </span>
+                            <?php endif ?>
+                            <div class='media-body'>
+							    <?php the_content(); ?>
+                            </div>
+                        </div>
+							<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+						<footer class="entry-meta">
+					</article><!-- #post-<?php the_ID(); ?> -->
+				
+							<?php comments_template( '', true ); ?>
+				
+					<?php endwhile; // end of the loop. ?>
+				</div>
+ 			 </div>
+			</div><!-- #content -->
+		</div><!-- #primary -->
+        <div class="push"></div>
+    </div><!-- #wrap -->
+<?php get_footer(); ?>
