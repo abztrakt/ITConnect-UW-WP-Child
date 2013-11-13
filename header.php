848234ec7374ed8ad0d793cdca1fdc4c126f4f94
Craig M. Stimmel <cstimmel@uw.edu>
2013-11-13 10:44:50 -0800

    Getting rid of http resource calls, so things work better in browsers over https.

--- header.php
+++ header.php
@@ -54,11 +54,11 @@
         }
      );
 </script>
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
 
 <!--
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>  -->
-<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<script src="//code.jquery.com/jquery-1.9.1.js"></script>  -->
+<script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script>
    $(function() {
      var path = "<?php bloginfo('stylesheet_directory'); ?>/keywords.txt";
