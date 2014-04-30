718af57f3fb69c1e3275c638145fb8e171a1ef50
Craig M. Stimmel <cstimmel@uw.edu>
2014-04-30 13:17:54 -0700

    Turn the JSON into a single record and print the short description as the h2.

--- page-itsm-detail.php
+++ page-itsm-detail.php
@@ -63,9 +63,14 @@
                         $response = wp_remote_get( $url, $args );
                         $body = wp_remote_retrieve_body( $response );
                         $JSON = json_decode( $body );
+                        $record = $JSON->records[0];
 
+                        echo "<h2>$record->short_description</h2>";
+
+
+                        echo "DEBUG: ";
                         echo "<pre>";
-                        var_dump($JSON);
+                        var_dump($record);
                         echo "</pre>";
                     }
                 ?>
