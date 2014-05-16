c4c4777167b129d32bbc5b49a32692be6217931f
Charlon Palacay <charlon@gmail.com>
2014-05-16 14:30:15 -0700

    Mostly working expect for broken navigation menu.

--- dropdowns.php
+++ dropdowns.php
@@ -1,13 +1,8 @@
-  <nav id="access" role="navigation" aria-label="Main menu">
+  <nav id="access" class="navbar navbar-default" role="navigation" aria-label="Main menu">
     <h3 class="assistive-text"><?php _e( 'Main menu', 'twentyeleven' ); ?></h3> <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
     <div class="it_container">
-    
-    
-    
-        <div id="navbar-menu" class="navbar" style="height:30px;">
-          <a class="btn btn-mini" id="help-button" href="<?php bloginfo('url'); ?>/help">Need Help?</a>
-          
-          
+        <div id="navbar-menu" class="navbar">
+          <a class="btn btn-xs" id="help-button" href="<?php bloginfo('url'); ?>/help">Need Help?</a>
           <a class="btn btn-navbar navbar-toggle" data-toggle="collapse" data-target=".nav-collapse" title="Open Navigation" href="#menu" tabindex="0" role="button" aria-haspopup="true">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -20,9 +15,6 @@
             <?php uw_dropdowns(); ?>
           </div>
         </div>
-        
-        
-        
     </div><!-- .container -->
 
   </nav><!-- #access -->
