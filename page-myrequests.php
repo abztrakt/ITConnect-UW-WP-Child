828ad500349f5bdf5a1109a9265d7bcf80387077
Cole Bessee <cbessee@uw.edu>
2014-10-07 16:32:23 -0700

    Watching tag no longer shows if both caller and watching

--- page-myrequests.php
+++ page-myrequests.php
@@ -213,7 +213,6 @@ if ( isset( $_SERVER['REMOTE_USER'] ) ) {
                     usort($req_jsonwl->records, 'sortByNumberDesc');
                     $req_count = 0;
                     foreach ( $req_json->records as $record ) {
-                    
                             if ($record->state != "Resolved" && $record->state != "Awaiting User Info") {
                                 $record->state = "Active";
                             }
