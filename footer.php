9e658397eb39157cba25d5f42d29e9754291d354
Kaiyu Zheng <qaz2wsx3@uw.edu>
2014-10-23 16:40:56 -0700

    Added logos:

--- footer.php
+++ footer.php
@@ -9,27 +9,40 @@
             <div id='footer_links' class='span9'>
                 <div id='footer_links_left' class='span4'>
                     <table>
+		      <tbody>
                         <tr>
-                            <td>Online</td><td><a href='<?php echo get_site_url();?>/it-connect-home/question'><?= $options['online'] ?></a></td>
+                            <td>Online</td><td><a href='<?php echo get_site_url();?>/it-connect-home/question'>Contact form</a></td>
                         </tr>
                         <tr>
-                            <td>Email</td><td><a href='mailto:<?= $options['email'] ?>'><?= $options['email'] ?></a></td>
+                            <td>Email</td><td><a href='mailto:<?= $options['email'] ?>'>help@uw.edu</a></td>
                         </tr>
+			<tr>
+			    <td>Phone</td><td>206-221-5000</td>
+			</tr>
+		      </tbody>
                     </table>
                 </div>
                 <div id='footer_links_mid' class='span4'>
                     <table>
+		      <tbody>                                        
                         <tr>
-                            <td>Phone</td><td><?= $options['phone'] ?></td>
+                            <td>In-Person</td><td><a href="http://www.washington.edu/maps/?l=UWTC">UW Tower, C-3000</a><br>M-F 8-8</td>
                         </tr>
-                        <tr>
-                            <td>In-Person</td><td><?= $options['inperson'] ?></td>
-                        </tr>
+			<tr>
+			   <td>Services</td><td><a href="http://depts.washington.edu/uwtscat">UW-IT Service Catalog</a></td>
+			</tr>
+		      </tbody>
                     </table>
                 </div>
                 <div id='footer_links_right' class='span4'>
-                    <p><a href='http://depts.washington.edu/uwtscat'>Service Catalog</a></p>
-                    <p><a href='http://depts.washington.edu/uwtscat/views/contactlist'>UW-IT Service Contact List</a></p>
+		  <table class="table">
+		   <tbody>		   
+                      <td>Connect With Us</td>
+		       <td><a href="http://twitter.com/uwitnews"><img src='<?php echo get_stylesheet_directory_uri(); ?>/img/twitter25.png' style="width: 18px;"> Twitter </a><br />
+		         <a href="http://youtube.com/user/uwinfotech/"><img src='<?php echo get_stylesheet_directory_uri(); ?>/img/youtube25.png' style="width: 18px;"> YouTube </a>
+			 </td> 
+		   </tbody>
+		  </table>
                 </div>
             </div>
             <div id='footer_logo_container' class='span3'>
