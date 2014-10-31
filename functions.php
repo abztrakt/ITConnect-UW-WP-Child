d002859a9dbae563a738146e3c8bf879dd77e814
Kaiyu Zheng <qaz2wsx3@uw.edu>
2014-10-31 16:50:50 -0700

    No specifc web extensions; Added https; Email validation remains because of consistency of prompting the user

--- functions.php
+++ functions.php
@@ -497,9 +497,10 @@ function section_cb() {
 }
 
 function set_online() {
-    $options = get_option('footer_options');
-    $url_pattern = '(http|ftp)://[a-zA-Z0-9_\-\.\+]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)([/a-zA-z0-9_\-\.\+\?=%]*)?';
+    $options = get_option('footer_options'); 
+    $url_pattern = '(http|https|ftp)://[a-zA-Z0-9_\-]+(\.{1}[a-zA-Z0-9_\-]*)?\.{1}[a-zA-Z0-9]+([/a-zA-z0-9_\-\+\?=%]*)?';
     $warning = 'Invalid URL format';
+   
     echo "<input name='footer_options[online]' pattern='$url_pattern' title='$warning' type='text' size='45' value='{$options['online']}' />";
 }
 
@@ -529,21 +530,21 @@ function set_inperson_hours() {
 
 function set_inperson_url() {
     $options = get_option('footer_options');
-    $url_pattern = '(http|ftp)://[a-zA-Z0-9_\-\.\+]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)([/a-zA-z0-9_\-\.\+\?=%]*)?';
+    $url_pattern = '(http|https|ftp)://[a-zA-Z0-9_\-\.\+]+\.[a-zA-Z0-9]+([/a-zA-z0-9_\-\.\+\?=%]*)?';
     $warning = 'Invalid URL format';
     echo "<input name='footer_options[inperson_url]'  pattern='$url_pattern' title='$warning'  type='text' size='45'  value='{$options['inperson_url']}' />";
 }
 
 function set_twitter() {
     $options = get_option('footer_options');
-    $url_pattern = '(http|ftp)://[a-zA-Z0-9_\-\.\+]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)([/a-zA-z0-9_\-\.\+\?=%]*)?';
+    $url_pattern = '(http|https|ftp)://[a-zA-Z0-9_\-\.\+]+\..[a-zA-Z0-9]+([/a-zA-z0-9_\-\.\+\?=%]*)?';
     $warning = 'Invalid URL format';
-    echo "<input name='footer_options[twitter]' pattern='(http|ftp)://[a-zA-Z0-9_\-\.\+]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)([/a-zA-z0-9_\-\.\+\?=%]*)?' title='Invalid URL' type='text' size='45' value='{$options['twitter']}' />";
+    echo "<input name='footer_options[twitter]' pattern='(http|ftp)://[a-zA-Z0-9_\-\.\+]+\.[a-zA-Z0-9]+([/a-zA-z0-9_\-\.\+\?=%]*)?' title='Invalid URL' type='text' size='45' value='{$options['twitter']}' />";
 }
 
 function set_youtube() {
     $options = get_option('footer_options');
-    $url_pattern = '(http|ftp)://[a-zA-Z0-9_\-\.\+]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)([/a-zA-z0-9_\-\.\+\?=%]*)?';
+    $url_pattern = '(http|https|ftp)://[a-zA-Z0-9_\-\.\+]+\.[a-zA-Z0-9]+([/a-zA-z0-9_\-\.\+\?=%]*)?';
     $warning = 'Invalid URL format';
     echo "<input name='footer_options[youtube]'  pattern='$url_pattern' title='$warning' type='text' size='45' value='{$options['youtube']}' />";
 }
