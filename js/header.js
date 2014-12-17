82fb041321418ad0e9f8d28513bd1cb1910c08e5
Kaiyu Zheng <qaz2wsx3@uw.edu>
2014-12-17 15:23:27 -0800

    Removed condition for login in to wp-admin

--- js/header.js
+++ js/header.js
@@ -250,11 +250,7 @@ jQuery(window).load(function() {
  */
 
     window.onhashchange = function() {
-        if( $('body').hasClass('admin-bar')) {
-            window.scrollBy(0, -60);
-        } else {
-            window.scrollBy(0, -30);
-        }
+        window.scrollBy(0, -30);
     }
 /*
  * Dropdowns
